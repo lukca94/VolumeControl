b59d634d6551b062acc477efa450f7b61a1bbcc0
Lukáš Mackových <lukas.mackovych@outlook.com>
2024-06-15 00:51:29 +0200

    update

--- GetSession.cs
+++ GetSession.cs
@@ -8,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace VolumeControl
 {
-    internal class GetSession
+    static class GetSessionEnumerator
     {
+        public static void GetAudioSessionEnumerator()
+        {
+            using (var sessionManager = GetDevice.GetDefaultAudioDevice())
+            {
+                using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
+                {
+                    foreach (var session in sessionEnumerator)
+                    {
+                        Console.WriteLine(session);
+                        using (var sessionControl = session.QueryInterface<AudioSessionControl2>()) //process name
+                        using (var simpleVolume = session.QueryInterface<SimpleAudioVolume>()) //process volume level
+                        {
+                            Console.WriteLine(sessionControl.Process.ToString()); //write process name
+                            Console.WriteLine(sessionControl.ProcessID.ToString());
+                            Console.WriteLine(simpleVolume.MasterVolume); //write process volume level
+
+                            //if (sessionControl.Process.ToString() == "System.Diagnostics.Process (chrome)")
+                            //    simpleVolume.MasterVolume = simpleVolume.MasterVolume + 0.05f; //fixnout max min range
+
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    static class GetSession
+    { 
+    
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,29 +11,9 @@ namespace VolumeControl
     {
         private static void Main(string[] args)
         {
-            using (var sessionManager = GetDevice.GetAudioDevice())
-            {
-                using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
-                {
-                    foreach (var session in sessionEnumerator)
-                    {
-                        using (var sessionControl = session.QueryInterface<AudioSessionControl2>()) // process name
-                        using (var simpleVolume = session.QueryInterface<SimpleAudioVolume>()) //process volume level
-                        {
-                            Console.WriteLine(sessionControl.Process.ToString()); //write process name
-                            Console.WriteLine(sessionControl.ProcessID.ToString());
-                            Console.WriteLine(simpleVolume.MasterVolume); //write process volume level
+            
 
-                            //if (sessionControl.Process.ToString() == "System.Diagnostics.Process (chrome)")
-                            //    simpleVolume.MasterVolume = simpleVolume.MasterVolume + 0.05f; //fixnout max min range
-
-                        }
-                    }
-                }
-            }
             Console.ReadKey();
         }
-
-
     }
 }
