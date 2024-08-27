0ce07bef694f28803b0710919ce9aca3aa20ff2a
Lukáš Mackových <lukca94@gmail.com>
2024-08-27 19:20:53 +0200

    basic encoder output

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -1,12 +1,29 @@
 #include <Arduino.h>
+#include <Encoder.h>
+
+#define TOP 3
+#define BOT 2
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	pinMode(TOP, INPUT_PULLUP);
+	pinMode(BOT, INPUT_PULLUP);
 }
 
 void loop()
 {
-	Serial.println("Hi");
-	delay(1000);	
+	Serial.println("_______");
+	if( digitalRead(TOP)==HIGH){
+		Serial.println("TOP HIGH");
+	}
+	else{
+		Serial.println("TOP LOW");
+	}
+	if( digitalRead(BOT)==HIGH){
+		Serial.println("BOT HIGH");
+	}
+	else{
+		Serial.println("BOT LOW");
+	}	
 }
