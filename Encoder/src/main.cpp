921de6a220adb2ceb4b5d7dc94a3327700cfa9f6
Lukáš Mackových <lukca94@gmail.com>
2024-08-28 15:11:03 +0200

    attach interupts

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -4,17 +4,22 @@
 #define TOP 3
 #define BOT 2
 
+int stateTop = HIGH;
+int stateBot = HIGH;
+
 void setup()
 {
 	Serial.begin(115200);
 	pinMode(TOP, INPUT_PULLUP);
 	pinMode(BOT, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(TOP), ChangeTop, CHANGE); //falling mb
+	attachInterrupt(digitalPinToInterrupt(BOT), ChangeBot, CHANGE);
 }
 
 void loop()
 {
 	Serial.println("_______");
-	if( digitalRead(TOP)==HIGH){
+	/* if( digitalRead(TOP)==HIGH){
 		Serial.println("TOP HIGH");
 	}
 	else{
@@ -25,5 +30,14 @@ void loop()
 	}
 	else{
 		Serial.println("BOT LOW");
-	}	
+	}	 */
+}
+
+void ChangeTop()
+{
+	stateTop = !stateTop;
+}
+void ChangeBot()
+{
+	stateBot = !stateBot;
 }
